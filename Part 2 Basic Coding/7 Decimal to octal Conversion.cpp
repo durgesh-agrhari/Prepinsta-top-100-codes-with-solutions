4f843ae3ec096a4c8671ebc66383a2ccc7957bb7
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-09-30 15:12:10 +0530

    Add files via upload

---
+++ Part 2 Basic Coding/7 Decimal to octal Conversion.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+//function to convert binary to decimal
+
+//main program
+
+int main()
+{
+    vector<int> v; int n; cin>>n;
+     int y; y=n;
+    
+    for(int i=1;i<=n;i=i*8)
+     {
+        int x = y%8; 
+       
+       if(y!=1)
+         y = y/8;
+
+        v.push_back(x);
+        
+     }
+
+     for(auto it=v.end(); it>=v.begin();it--)
+       {
+           cout<<*it;
+       }
+    return 0;
+}
---
+++ Part 2 Basic Coding/9 Octal to Decimal conversion.cpp
@@ -0,0 +1,32 @@
+    //Octal to decimal conversion
+    #include <iostream>
+    #include <math.h>
+    using namespace std;
+    // Function to convert octal number to decimal
+    int convert(int octal)
+    {
+        int decimal = 0, i = 0;
+        //converting octal to decimal
+        while (octal != 0)
+        {
+            int rem = octal % 10;
+            octal /= 10;
+            int res=rem*pow(8,i);
+            decimal += res;
+            i++;
+        }
+        return decimal;
+    }
+    //main program
+    int main()
+    {
+        int octal;
+        //cout << “Enter an octal number: “;
+        //user input
+        cin >> octal;
+        //calling function
+        int decimal=convert(octal);
+        //printing output
+        cout << octal<<" " << decimal ;     
+        return 0;
+    }
