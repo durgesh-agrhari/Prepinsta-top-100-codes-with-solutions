43ecf0207ca3294cc759c2a198b4b6143e1dc515
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-10-05 22:28:02 +0530

    Add files via upload

---
+++ Part 2 Basic Coding/18 Check whether a character is a alphabet or not.cpp
@@ -0,0 +1,27 @@
+//Character is Alphabet or not
+#include<iostream>
+using namespace std;
+
+//main program
+int main()
+{
+
+char alpha;
+cout<<"Enter a character: "; 
+cin>>alpha;
+
+//checking for alphabet using ASCII value
+if((alpha>=65 && alpha<=90)||(alpha>=97 && alpha<=122))
+{
+
+//input lies in range
+cout<<alpha<<" is an alphabet ";
+}
+
+else
+{
+//input does not lie in range 
+cout<<alpha<<" is not an alphabet ";
+}
+return 0;
+}
---
+++ Part 2 Basic Coding/20 Find the ASCII value of a character .cpp
@@ -0,0 +1,17 @@
+//C++ program to calcualte ASCII value of Character
+#include<iostream>
+using namespace std;
+
+//main program
+int main()
+{
+	char val;
+	cout<<"Enter a character: ";
+	cin>>val;
+
+//printing the ASCII value of input 
+ //through typecasting
+
+cout<<"The ASCII value of "<<val<<" is "<<(int)val;
+return 0;
+}
