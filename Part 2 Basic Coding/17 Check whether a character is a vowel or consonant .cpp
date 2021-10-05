6d3fba4f703d1c38c8e4d80cd200a89c02a87a32
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-10-05 22:27:32 +0530

    Add files via upload

---
+++ Part 2 Basic Coding/17 Check whether a character is a vowel or consonant .cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char s;
+    cin >> s;
+    if (s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u')
+    {
+        cout << "Vowel";
+    }
+    else
+    {
+        cout << "consonant";
+    }
+
+    return 0;
+}
