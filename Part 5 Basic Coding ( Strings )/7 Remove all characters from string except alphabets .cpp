9bfe621e6e592436b43da13e89e1bdce67662ce2
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-10-29 22:49:43 +0530

    Add files via upload

---
+++ Part 5 Basic Coding ( Strings )/7 Remove all characters from string except alphabets .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Initializing variable.
+    char str[100];
+    int i, j;
+    gets(str);
+
+    //Iterating each character and removing non alphabetical characters.
+    for(i = 0; str[i] != '\0'; ++i)
+    {
+        while (!( (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || str[i] == '\0') )
+        {
+            for(j = i; str[j] != '\0'; ++j)
+            {
+                str[j] = str[j+1];
+            }
+            str[j] = '\0'; 
+        }
+    }
+    cout<<"After removing non alphabetical : "; 
+    puts(str);
+    return 0;
+}
