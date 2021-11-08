5fddc456533cb3258b51bad2e57d2a4e1c853edc
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-11-08 07:13:55 +0530

    Add files via upload

---
+++ Part 5 Basic Coding ( Strings )/11 Capitalize the first and last character of each word of a string.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctype.h>
+#include <string.h>
+using namespace std;
+
+int main() 
+{
+ 
+  //Initializing variables.
+  char str[100];
+  int length = 0;
+ 
+  //Accepting inputs.
+  //cout<<"Enter a string:\n";
+  gets(str);
+  
+  //Calculating length.
+  length = strlen(str);
+  
+  for(int i=0;i<length;i++)
+  {
+      if(i==0||i==(length-1)) //Conerting character at first and last index to uppercase.
+      {
+          str[i]=toupper(str[i]);
+      }
+      else if(str[i]==' ')//Conerting characters present before and after space to uppercase.
+      {
+          str[i-1]=toupper(str[i-1]);
+          str[i+1]=toupper(str[i+1]);
+          
+      }
+  }
+  
+  //Printing result.
+  cout<<"String after capitalizing first and last letter of each word:\n"<<endl<<str;
+ 
+  return 0;
+}
